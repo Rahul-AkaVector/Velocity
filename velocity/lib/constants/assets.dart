80eaeacbce224bdfe9c674867f9d4c7f6f44aec7
Rahul-AkaVector <rahulpathania160404@gmail.com>
2023-10-21 23:50:01 +0530

    assets file instance added

---
+++ velocity/lib/constants/assets.dart
@@ -0,0 +1,17 @@
+class AssetsImages {
+  static AssetsImages instance = AssetsImages();
+
+  static const String _imagesPath = "assets/images";
+
+  String javascriptImage = "$_imagesPath/courses/js.png";
+  String welcomeImage = "$_imagesPath/welcome/welcome.png";
+}
+
+class AssetsFonts {
+  static AssetsFonts instance = AssetsFonts();
+
+  static const String _fontsPath = "assets/fonts";
+
+  String gothamFont = "$_fontsPath/Gotham-Black.otf";
+  String heavitasFont = "$_fontsPath/Heavitas.ttf";
+}
