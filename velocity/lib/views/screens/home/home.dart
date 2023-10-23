a90551dc90ec774b1f6d5945c4d34bb7fe487ebb
Rahul-AkaVector <rahulpathania160404@gmail.com>
2023-10-23 23:27:11 +0530

    List View Builder data check

--- velocity/lib/views/screens/home/home.dart
+++ velocity/lib/views/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:velocity/constants/assets.dart';
 // import 'package:velocity/constants/assets.dart';
 import 'package:velocity/views/widgets/cards.dart';
 import 'package:velocity/views/widgets/titles/section_title.dart';
@@ -15,88 +16,146 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        // physics: AlwaysScrollableScrollPhysics(),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 20,
+      scrollDirection: Axis.vertical,
+      // physics: AlwaysScrollableScrollPhysics(),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              height: 200,
+              width: 400,
+              decoration: BoxDecoration(
+                  color: Colors.deepPurpleAccent.shade100,
+                  borderRadius: BorderRadius.circular(15)),
             ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Container(
-                height: 200,
-                width: 400,
-                decoration: BoxDecoration(
-                    color: Colors.deepPurpleAccent.shade100,
-                    borderRadius: BorderRadius.circular(15)),
-              ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SectionTitle(title: "Continue.."),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                RectangleBanner(),
+                RectangleBanner(),
+                RectangleBanner(),
+              ],
             ),
-            SizedBox(
-              height: 20,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SectionTitle(title: "Trending"),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                SquareBanner(),
+                SquareBanner(),
+                SquareBanner(),
+                SquareBanner(),
+                SquareBanner(),
+              ],
             ),
-            SectionTitle(title: "Continue.."),
-            SingleChildScrollView(
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SectionTitle(title: "Start Your Journey"),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                SquareBanner(),
+                SquareBanner(),
+                SquareBanner(),
+                SquareBanner(),
+                SquareBanner(),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SectionTitle(title: "Explore Web Dev"),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                SquareBanner(),
+                SquareBanner(),
+                SquareBanner(),
+                SquareBanner(),
+                SquareBanner(),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SectionTitle(title: "Explore Backend"),
+          SizedBox(
+            height: 130,
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: 6,
               scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  RectangleBanner(),
-                  RectangleBanner(),
-                  RectangleBanner(),
-                ],
-              ),
+              itemBuilder: (_, index) {
+                return GestureDetector(
+                  onTap: () {
+                    
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 20),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 80,
+                          height: 80,
+                          padding: EdgeInsets.all(8.0),
+                          decoration: BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: Image.asset(AssetsImages.instance.jsImage,
+                                fit: BoxFit
+                                    .cover // Ensure the image covers the entire area
+                                ),
+                          ),
+                        ),
+                
+                        //text
+                        SizedBox(
+                          height: 15,
+                        ),
+                
+                        SizedBox(
+                          width: 79,
+                          child: Text(
+                            'Python Backend',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              },
             ),
-            SizedBox(
-              height: 20,
-            ),
-            SectionTitle(title: "Trending"),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  SquareBanner(),
-                  SquareBanner(),
-                  SquareBanner(),
-                  SquareBanner(),
-                  SquareBanner(),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            SectionTitle(title: "Start Your Journey"),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  SquareBanner(),
-                  SquareBanner(),
-                  SquareBanner(),
-                  SquareBanner(),
-                  SquareBanner(),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            SectionTitle(title: "Explore Web Dev"),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  SquareBanner(),
-                  SquareBanner(),
-                  SquareBanner(),
-                  SquareBanner(),
-                  SquareBanner(),
-                ],
-              ),
-            ),
-          ],
-        ),
-      );
+          ),
+        ],
+      ),
+    );
   }
 }
