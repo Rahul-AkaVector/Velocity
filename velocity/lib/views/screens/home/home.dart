084ffb0bdbb1d469de28456607fcc28e823ddd8b
Rahul-AkaVector <rahulpathania160404@gmail.com>
2023-10-26 02:12:09 +0530

    Loading Screen  Added

--- velocity/lib/views/screens/home/home.dart
+++ velocity/lib/views/screens/home/home.dart
@@ -16,6 +16,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<CourseListModel> coursesList = [];
+  bool isLoading = false;
 
   @override
   void initState() {
@@ -24,179 +25,197 @@ class _HomeState extends State<Home> {
   }
 
   void getCategoryList() async {
+    setState(() {
+      isLoading = true;
+    });
+
     coursesList = await FirebaseFirestoreHelper.instance.getList();
     coursesList.shuffle();
-    print(coursesList);
+
+    setState(() {
+      isLoading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.vertical,
-      // physics: AlwaysScrollableScrollPhysics(),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
+    return isLoading
+        ? Center(
             child: Container(
-              height: 200,
-              width: 400,
-              decoration: BoxDecoration(
-                  color: Colors.deepPurpleAccent.shade100,
-                  borderRadius: BorderRadius.circular(15)),
+              height: 100,
+              width: 100,
+              alignment: Alignment.center,
+              child: CircularProgressIndicator(),
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          SectionTitle(title: "Continue.."),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
+          )
+        : SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            // physics: AlwaysScrollableScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                RectangleBanner(),
-                RectangleBanner(),
-                RectangleBanner(),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          SectionTitle(title: "Trending"),
-          coursesList.isEmpty
-              ? Center(
-                  child: Text(
-                    "Empty List",
-                  ),
-                )
-              : SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: coursesList
-                        .map(
-                          (e) => Padding(
-                            padding: const EdgeInsets.fromLTRB(10, 5, 5, 5),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(
-                                  10), // Same as Card's borderRadius
-                              child: SizedBox(
-                                height: 110,
-                                width: 110,
-                                // child: Image.asset(
-                                //   AssetsImages.instance.jsImage,
-                                //   fit: BoxFit
-                                //       .fill, // Ensure the image covers the entire area
-                                // ),
-                                child: Container(
-                                  decoration:
-                                      BoxDecoration(color: Colors.white),
-                                  child: Image.network(
-                                    e.image,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        )
-                        .toList(),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    height: 200,
+                    width: 400,
+                    decoration: BoxDecoration(
+                        color: Colors.deepPurpleAccent.shade100,
+                        borderRadius: BorderRadius.circular(15)),
                   ),
                 ),
-          SizedBox(
-            height: 20,
-          ),
-          SectionTitle(title: "Start Your Journey"),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                SquareBanner(),
-                SquareBanner(),
-                SquareBanner(),
-                SquareBanner(),
-                SquareBanner(),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          SectionTitle(title: "Explore Web Dev"),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                SquareBanner(),
-                SquareBanner(),
-                SquareBanner(),
-                SquareBanner(),
-                SquareBanner(),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          SectionTitle(title: "Explore Backend"),
-          SizedBox(
-            height: 130,
-            child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: 6,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (_, index) {
-                return GestureDetector(
-                  onTap: () {},
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 20),
-                    child: Column(
-                      children: [
-                        Container(
-                          width: 80,
-                          height: 80,
-                          padding: EdgeInsets.all(8.0),
-                          decoration: BoxDecoration(
-                            color: Colors.amber,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Center(
-                            child: Image.asset(AssetsImages.instance.jsImage,
-                                fit: BoxFit
-                                    .cover // Ensure the image covers the entire area
-                                ),
-                          ),
-                        ),
-
-                        //text
-                        SizedBox(
-                          height: 15,
-                        ),
-
-                        SizedBox(
-                          width: 79,
-                          child: Text(
-                            'Python Backend',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        )
-                      ],
-                    ),
+                SizedBox(
+                  height: 20,
+                ),
+                SectionTitle(title: "Continue.."),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      RectangleBanner(),
+                      RectangleBanner(),
+                      RectangleBanner(),
+                    ],
                   ),
-                );
-              },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SectionTitle(title: "Trending"),
+                coursesList.isEmpty
+                    ? Center(
+                        child: Text(
+                          "Empty List",
+                        ),
+                      )
+                    : SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children: coursesList
+                              .map(
+                                (e) => Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(10, 5, 5, 5),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(
+                                        10), // Same as Card's borderRadius
+                                    child: SizedBox(
+                                      height: 110,
+                                      width: 110,
+                                      // child: Image.asset(
+                                      //   AssetsImages.instance.jsImage,
+                                      //   fit: BoxFit
+                                      //       .fill, // Ensure the image covers the entire area
+                                      // ),
+                                      child: Container(
+                                        decoration:
+                                            BoxDecoration(color: Colors.white),
+                                        child: Image.network(
+                                          e.image,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              )
+                              .toList(),
+                        ),
+                      ),
+                SizedBox(
+                  height: 20,
+                ),
+                SectionTitle(title: "Start Your Journey"),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      SquareBanner(),
+                      SquareBanner(),
+                      SquareBanner(),
+                      SquareBanner(),
+                      SquareBanner(),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SectionTitle(title: "Explore Web Dev"),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      SquareBanner(),
+                      SquareBanner(),
+                      SquareBanner(),
+                      SquareBanner(),
+                      SquareBanner(),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SectionTitle(title: "Explore Backend"),
+                SizedBox(
+                  height: 130,
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: 6,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (_, index) {
+                      return GestureDetector(
+                        onTap: () {},
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 20),
+                          child: Column(
+                            children: [
+                              Container(
+                                width: 80,
+                                height: 80,
+                                padding: EdgeInsets.all(8.0),
+                                decoration: BoxDecoration(
+                                  color: Colors.amber,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Center(
+                                  child: Image.asset(
+                                      AssetsImages.instance.jsImage,
+                                      fit: BoxFit
+                                          .cover // Ensure the image covers the entire area
+                                      ),
+                                ),
+                              ),
+
+                              //text
+                              SizedBox(
+                                height: 15,
+                              ),
+
+                              SizedBox(
+                                width: 79,
+                                child: Text(
+                                  'Python Backend',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
-    );
+          );
   }
 }
 
