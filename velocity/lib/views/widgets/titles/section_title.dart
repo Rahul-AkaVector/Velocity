4a636d7b3d1bd4051091aa3b58b2417546f3e101
Rahul-AkaVector <rahulpathania160404@gmail.com>
2023-10-28 00:04:19 +0530

    Updated

--- velocity/lib/views/widgets/titles/section_title.dart
+++ velocity/lib/views/widgets/titles/section_title.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:google_fonts/google_fonts.dart';
 import 'package:velocity/constants/assets.dart';
 
 class SectionTitle extends StatelessWidget {
